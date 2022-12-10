9906b4a37f8e58434b3cb0e12b670431e68ca604
teamriders098 <111563785+teamriders098@users.noreply.github.com>
2022-12-11 01:39:08 +0530

    Update linux-userland-pc.sh

--- linux-userland-pc.sh
+++ linux-userland-pc.sh
@@ -61,13 +61,13 @@ ls
 
 yarn install 
 
-sudo apt-get install curl
+# sudo apt-get install curl
 
 
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+# curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 
 
-sudo apt-get install -y nodejs
+# sudo apt-get install -y nodejs
 
 yarn add sharp
 
